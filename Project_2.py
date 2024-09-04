07570b2b37d08c92b32d8fc11f3a9bda057f414f
Habib Ur Rehman <habibrehman96@outlook.com>
2024-09-04 19:45:43 +0500

    Update Project_2.py

--- Project_2.py
+++ Project_2.py
@@ -1,22 +1,24 @@
+
 student_grades={ }
 
-#add a new student
-def add_student(name,grade):
-    student_grades[name]=grade
+def add_student(name, grade):
+    student_grades[name] = grade
     print(f"Added {name} with a {grade}")
 
-def update_student(name,grade):
+def update_student(name, grade):
     if name in student_grades:
-        student_grades[name]=grade
+        student_grades[name] = grade
+       
         print(f"{name} with marks are updated {grade}" )
+    
     else:
         print(f"{name} is not found!")
-def delete_student(name, grade):
+def delete_student(name):
     if name in student_grades:
         del student_grades[name]
         print(f"{name} has been successfuly deleted!")
     else:
-         print(f"{name} is not found!")
+         print("{name} is not found!")
 
 def display_all_students():
     if student_grades:
@@ -24,17 +26,43 @@ def display_all_students():
             print(f"{name} : {grade}")
 
     else:
-        print("No student found/added")
+         print("No student found/added")
 
 def main():
-    while True:
-        print('\n Taqwa Students Grades Management System')
+     while True:
+        print("Taqwa Students Grades Management System")
         print("1. Add Student")
         print("2. update Student")
         print("3. delete Student")
         print("4. view Student")
         print("5. exit Student")
-
         
+        choice=int(input("Enter your Choice="))
+        if choice == 1:
+          
+            name=(input("Enter your name="))
+            grade=int(input("Enter yoyr grade="))
+            add_student(name,grade)
+       
+        elif choice == 2:
+          
+            name=(input("Enter your name="))
+            grade=int(input("Enter yoyr grade="))
+            update_student(name,grade)
+       
+        elif choice == 3:
+            name=(input("Enter your name="))
+            delete_student(name)
+       
+        elif choice == 4:
+            display_all_students()
+       
+        elif choice == 5:
+            print("Closing the program..")
+            break  
+      
+        else:
+            print("Invalid choice")
+    
 
 
